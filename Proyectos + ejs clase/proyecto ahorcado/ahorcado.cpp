b8055abe7ae33b9d8a7809a0671f73890803ab95
Marina Jun Carranza Sánchez <104431116+marinajcs@users.noreply.github.com>
2022-11-11 17:01:17 +0100

    Update ahorcado.cpp

--- Proyectos + ejs clase/proyecto ahorcado/ahorcado.cpp
+++ Proyectos + ejs clase/proyecto ahorcado/ahorcado.cpp
@@ -2,55 +2,55 @@
   * @file ahorcado.cpp
   * @brief Programa para jugar al ahorcado en consola
   *
-  * @author Pedro PalacÌn Ruiz de la Escalera, Marina Jun Carranza S·nchez
+  * @author Marina Jun Carranza S√°nchez
   * @date Enero-2020
   *
-  * El programa implementa una versiÛn b·sica del juego del ahorcado como
+  * El programa implementa una versi√≥n b√°sica del juego del ahorcado como
   * ejercicio de uso de clases simples.
   *
   * El objetivo que se pretende es programar el juego en base a una clase "Ahorcado"
   * que resuelva las operaciones que se tienen que realizar. El programa principal
-  * deber· declarar un objeto de esta clase y llamar a los distintos mÈtodos hasta
+  * deber√° declarar un objeto de esta clase y llamar a los distintos m√©todos hasta
   * el final de la partida.
   *
-  * Para hacerse una idea de quÈ componentes formar·n parte de la clase:
+  * Para hacerse una idea de qu√© componentes formar√°n parte de la clase:
   *
-  * - Un objeto de esta clase, deber· tener informaciÛn sobre:
+  * - Un objeto de esta clase, deber√° tener informaci√≥n sobre:
   *    - La frase o palabra que hay que acertar.
   *    - La plantilla con el estado actual de la partida (la palabra con guiones).
-  *    - El n˙mero de intentos que quedan.
+  *    - El n√∫mero de intentos que quedan.
   *    - Las letras que ya se han dicho hasta el momento.
   *
-  * - Para que la clase controle el proceso de la partida, deber· tener operaciones para:
+  * - Para que la clase controle el proceso de la partida, deber√° tener operaciones para:
   *    - Consultar el estado actual de la plantilla (la cadena de letras/guiones actual)
-  *    - Consultar el n˙mero de intentos que quedan.
+  *    - Consultar el n√∫mero de intentos que quedan.
   *    - Consultar la cadena de letras que ya se han usado.
   *    - Saber si la partida se ha terminado.
   *    - Saber si una letra se puede decir o no.
   *    - Saber si se ha ganado la partida.
   *    - Procesar una nueva letra seleccionada por el jugador, teniendo en cuenta que si
-  *      se da en may˙scula tambiÈn debe funcionar. La funciÛn devolver· si se ha procesado
-  *      correctamente, es decir, si la letra tenÌa sentido y no se habÌa usado antes. Por
-  *      ejemplo, si le da un car·cter que no es una letra no se puede procesar.
+  *      se da en may√∫scula tambi√©n debe funcionar. La funci√≥n devolver√° si se ha procesado
+  *      correctamente, es decir, si la letra ten√≠a sentido y no se hab√≠a usado antes. Por
+  *      ejemplo, si le da un car√°cter que no es una letra no se puede procesar.
   *
-  * Para hacer el programa m·s interesante, el juego deberÌa "inventarse" una palabra. Para
+  * Para hacer el programa m√°s interesante, el juego deber√≠a "inventarse" una palabra. Para
   * resolver esto, creamos una clase con la responsabilidad de seleccionar una palabra
-  * aleatoria. El diseÒo que se propone es crear una nueva clase "Bolsa" que nos hace de
+  * aleatoria. El dise√±o que se propone es crear una nueva clase "Bolsa" que nos hace de
   * generador aleatorio.
   *
-  * Para hacerse una idea de quÈ componenetes formar·n parte de la clase, tenga en cuenta
-  * que deber· tener m˙ltiples palabras y nos deberÌa permitir "sacar palabras" en un orden
+  * Para hacerse una idea de qu√© componenetes formar√°n parte de la clase, tenga en cuenta
+  * que deber√° tener m√∫ltiples palabras y nos deber√≠a permitir "sacar palabras" en un orden
   * arbitrario. Para ello, puede considerar
-  *     - Deber· contener un vector privado con las palabras que hay en la bolsa.
-  *     - El constructor deberÌa cargar ese vector privado con m˙ltiples palabras en un
+  *     - Deber√° contener un vector privado con las palabras que hay en la bolsa.
+  *     - El constructor deber√≠a cargar ese vector privado con m√∫ltiples palabras en un
   *       orden aleatorio.
-  *     - DeberÌa tener un mÈtodo para seleccionar una nueva palabra.
+  *     - Deber√≠a tener un m√©todo para seleccionar una nueva palabra.
   *
   * Ya que es una bolsa, podemos realizar el siguiente comportamiento:
   *     - Cuando se declara un objeto de la bolsa, se cargan las palabras y se barajan.
   *     - Se puede pedir la siguiente palabra, dando lugar a una secuencia de palabras que
-  *       surgen con un orden aleatorio seg˙n hayan quedado ordenadas al construir la bolsa.
-  *     - Si se llegan a pedir todas las palabras, pedir la siguiente palabra implicar· volver
+  *       surgen con un orden aleatorio seg√∫n hayan quedado ordenadas al construir la bolsa.
+  *     - Si se llegan a pedir todas las palabras, pedir la siguiente palabra implicar√° volver
   *       a barajar la bolsa y comenzar con la primera de ellas.
   *
   * Para simplificar el problema sin entrar en soluciones que impliquen pedir palabras desde
@@ -76,18 +76,18 @@
   *       "ejercicio",  "hacienda",   "posadero",   "Rocinante",
   *       "presencia",  "historias",  "presentes",  "verdadero"
   *
-  * Observe que una vez que tenga las dos clases, puede declarar una bolsa de palabras y despuÈs
+  * Observe que una vez que tenga las dos clases, puede declarar una bolsa de palabras y despu√©s
   * inicializar un objeto de la clase Ahorcado con una palabra aleatoria, ya que la palabra se pide
   * al objeto "Bolsa".
   *
   * Para programar el juego, puede definir la clase "Ahorcado" e inicializar el objeto con una palabra
   * fija y conocida (por ejemplo, en el constructor). Una vez que ya lo ha depurado y obtenido
-  * una soluciÛn que funciona, puede aÒadir la clase bolsa y crear un programa que juega varias
+  * una soluci√≥n que funciona, puede a√±adir la clase bolsa y crear un programa que juega varias
   * partidas del ahorcado.
   *
-  * En concreto, el programa pedir· cu·ntas palabras quiere adivinar y repetir· el juevo con un
-  * bucle que permita al usuario jugar varias partidas. Note que declarar· una Bolsa al principio del
-  * main y el bucle que repite las partidas pedir· a dicha bolsa una nueva palabra para cada nueva partida.
+  * En concreto, el programa pedir√° cu√°ntas palabras quiere adivinar y repetir√° el juevo con un
+  * bucle que permita al usuario jugar varias partidas. Note que declarar√° una Bolsa al principio del
+  * main y el bucle que repite las partidas pedir√° a dicha bolsa una nueva palabra para cada nueva partida.
   *
   */
 #include <iostream>
@@ -201,7 +201,7 @@ private:
 public:
     Ahorcado(string palabra)
     {
-        //Constructor. AÒadir palabra aleatoria de "bolsa"
+        //Constructor. A√±adir palabra aleatoria de "bolsa"
         palabra_random = palabra;
         num_letras = palabra_random.size();
 
@@ -257,7 +257,7 @@ public:
     void EstadoPartida()
     {
         if (PartidaGanada() && !PartidaFinalizada()){
-            cout << "°Ha ganado! :D (Partida finalizada) " << endl;
+            cout << "¬°Ha ganado! :D (Partida finalizada) " << endl;
             cout << "Palabra:" << palabra_random << endl;
         }else if (!PartidaGanada() && PartidaFinalizada()){
             cout << "Game over... :( (Partida finalizada) " << endl;
@@ -281,7 +281,7 @@ public:
             intentos--;
         }
         else
-            cout << "El car·cter introducido no es v·lido." << endl;
+            cout << "El car√°cter introducido no es v√°lido." << endl;
     }
 
 };
